47a428fa2de48a8d852a3f32e16d147fc76e7c3a
DesarrolladoresXX <NPicci19@gmail.com>
2022-02-21 16:13:39 -0300

    Desafio de ciclos

---
+++ main.js
@@ -0,0 +1,18 @@
+// ciclo que imprime del 0 al 20 saltando en dos en dos
+
+let i = 0
+
+while (i <= 20) {
+    alert(i);
+    i = i + 2;
+}
+
+/* lo mismo pero con "for"
+
+let i = 0
+
+for (let i=0; i <= 20; i = i+2){
+    alert(i);
+}
+*/
+
